6b97b4d307af5048240be8c82c9bfd8129441b2e
Erik Jälevik <erik.jalevik@futurice.com>
2017-07-28 15:31:26 +0200

    Add reactstrap (with a CDN link to actual bootstrap in head)

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,13 @@
+import Head from 'next/head';
 import Header from './Header';
 
 const Layout = (props) => (
   <div>
+    <Head>
+      <title>Masifunde</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+    </Head>
     <Header />
     {props.children}
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import { Button } from 'reactstrap';
 
 const Index = (props) => (
   <Layout>
     <h1>Masifunde</h1>
     <p>{props.text}</p>
+    <Button color='danger'>I am a Reactstrap button</Button>
   </Layout>
 );
 
