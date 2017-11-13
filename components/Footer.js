70040c92551c2a4c214011bbafb87b1102656e10
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-11-13 10:19:01 +0100

    Update footer links/sitemap

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import Link from 'next/link'
 import { Container, Row, Col } from 'reactstrap'
-import FaTwitter from 'react-icons/lib/fa/twitter'
 import FaYoutubePlay from 'react-icons/lib/fa/youtube-play'
 import FaFacebook from 'react-icons/lib/fa/facebook'
+
+import { Link, RouteNames } from '../routes'
 
 const containerStyle = {
   marginTop: 20,
@@ -45,17 +45,50 @@ export default function Footer() {
             <div className="row">
               <div className="col">
                 <ul className="list-unstyled">
-                  <li>Was wir machen</li>
-                  <li>Approach</li>
-                  <li>Impact</li>
+                  <li>
+                    <Link route={RouteNames.WhatWeDo} passHref>
+                      <a>Was wir machen</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.ApproachSA} passHref>
+                      <a>Approach South Africa</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.ApproachDE} passHref>
+                      <a>Approach Deutchland</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.Impact} passHref>
+                      <a>Impact</a>
+                    </Link>
+                  </li>
                 </ul>
               </div>
               <div className="col">
                 <ul className="list-unstyled">
-                  <li>Wer sind wir</li>
-                  <li>Team South Africa</li>
-                  <li>Team Deutschland</li>
-                  <li>Partners</li>
+                  <li>
+                    <Link route={RouteNames.WhoWeAre} passHref>
+                      <a>Wer sind wir</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.TeamSA} passHref>
+                      <a>Team South Africa</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.TeamDE} passHref>
+                      <a>Team Deutschland</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.Partners} passHref>
+                      <a>Partners</a>
+                    </Link>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -65,22 +98,40 @@ export default function Footer() {
             <div className="row">
               <div className="col">
                 <ul className="list-unstyled">
-                  <li>Wie Sie helfen</li>
-                  <li>Spenden</li>
-                  <li>Become volunteer</li>
-                  <li>Open positions</li>
+                  <li>
+                    <Link route={RouteNames.HowToSupport} passHref>
+                      <a>Wie Sie helfen</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.Donate} passHref>
+                      <a>Spenden</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.BecomeSponsor} passHref>
+                      <a>Sponsor</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.BecomeVolunteer} passHref>
+                      <a>Volunteer</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link route={RouteNames.BecomePartner} passHref>
+                      <a>Open Partner</a>
+                    </Link>
+                  </li>
                 </ul>
               </div>
               <div className="col">
                 <ul className="list-unstyled">
-                  <li>Blog</li>
                   <li>
-                    <Link href="/contact">
+                    <Link route={RouteNames.Contact} passHref>
                       <a>Kontakt</a>
                     </Link>
                   </li>
-                  <li>Presse</li>
-                  <li>Newsletter</li>
                 </ul>
               </div>
             </div>
@@ -92,7 +143,6 @@ export default function Footer() {
         <Row>
           <Col>
             <h4>
-              <FaTwitter style={socialStyle} />
               <FaYoutubePlay style={socialStyle} />
               <FaFacebook style={socialStyle} />
             </h4>
@@ -100,7 +150,7 @@ export default function Footer() {
         </Row>
         <Row>
           <Col style={copyrightRowStyle}>
-            Copyright 2017 Masifunde Learner Development
+            © 2017 Masifunde Learner Development
           </Col>
         </Row>
         <Row>
