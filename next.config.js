529b49c43b4f1c64fdacb074f129a25872a01d44
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-11-14 15:32:07 +0100

    Fix index page generation

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   exportPathMap() {
     return {
-      '/': { page: '/' },
+      '/': { page: '/home' },
       // '/en/waswirmachen': { page: '/waswirmachen', query: { locale: 'en' } },
     }
   },
