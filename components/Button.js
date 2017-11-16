bb46b289ddb27c0cabdc7183535fa1f994359a81
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-11-16 10:17:37 +0100

    Create Button component

---
+++ components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+function CustomButton(props) {
+  const { children, className } = props
+  return (
+    <a {...props} className={`btn btn-primary ${className}`}>
+      {children}
+    </a>
+  )
+}
+
+CustomButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string.isRequired,
+}
+
+const StyledButton = styled(CustomButton)`
+  background-color: black;
+  border-color: black;
+  color: white !important;
+`
+
+export default StyledButton
