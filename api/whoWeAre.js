e26be978de18ce5631ae5ec9ab989eb584f18145
Torkjel Skjetne <torkjel.skjetne@futurice.com>
2017-11-21 14:08:01 +0100

    Wireframe for Team DE page

--- api/whoWeAre.js
+++ api/whoWeAre.js
@@ -1,6 +1,32 @@
-/* eslint-disable import/prefer-default-export */
 import { fetchSingleEntry } from './contentfulService'
 
 export async function fetchWhoWeArePage(locale) {
   return fetchSingleEntry('about', locale)
+}
+
+const unwrapImage = image => ({
+  url: image.fields.file.url,
+  title: image.fields.title,
+})
+
+const unwrapRegion = region => region.fields.name
+
+const unwrapTeamMembers = (teamMembers = []) =>
+  teamMembers.map((teamMember) => {
+    const { fields } = teamMember
+    return {
+      id: teamMember.sys.id,
+      name: fields.name,
+      image: unwrapImage(fields.profileImage),
+      region: unwrapRegion(fields.region),
+    }
+  })
+
+export async function fetchTeamDePage(locale) {
+  const content = await fetchSingleEntry('pageTeamDe', locale)
+  return {
+    ...content,
+    sectionOneImage: unwrapImage(content.sectionOneImage),
+    teamMembers: unwrapTeamMembers(content.teamMembers),
+  }
 }
--- pages/who-we-are/team-de.js
+++ pages/who-we-are/team-de.js
@@ -1,15 +1,109 @@
 import React from 'react'
-import { Container, Row } from 'reactstrap'
+import { Container } from 'reactstrap'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
+import Markdown from '../../components/Markdown'
 import Layout from '../../components/Layout'
+import { fetchTeamDePage } from '../../api/whoWeAre'
+import { getLocaleFromQuery } from '../../utils/locale'
+import WithHeaderAndFooterData from '../../components/WithHeaderAndFooterData'
+import Banner from '../../components/Banner'
+import TeamMember from '../../components/TeamMember'
 
-const TeamDe = () => (
+const LocationsList = styled.ul`
+  column-count: 2;
+  list-style-type: none;  
+  margin-bottom: 0;
+`
+
+const LocationListItem = styled.li`
+  padding: 0.6rem 2rem;
+  padding-left: 0;  
+`
+
+const TeamMemberList = styled.div`
+  margin: 2rem 0 2.5rem 0;
+`
+
+const ImageContainer = styled.div`
+  margin: 3rem 0 4.5rem 0;
+`
+
+const Paragraph = styled(Markdown)`
+  text-align: center;
+`
+
+const TeamDe = ({
+  title,
+  pageDescription,
+  bannerTitle,
+  bannerButtonText,
+  sectionOneTitle,
+  sectionOneText,
+  sectionTwoTitle,
+  sectionOneList,
+  sectionOneImage,
+  teamMembers,
+}) => (
   <Layout>
     <Container>
-      <Row>Empty page</Row>
+      <h1>{title}</h1>
+      <Paragraph source={pageDescription} />
+      <section>
+        <h2>{sectionOneTitle}</h2>
+        <Paragraph source={sectionOneText} />
+        <ImageContainer className="d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center">
+          <img className="col-sm-12 col-md-6" src={sectionOneImage.url} alt={sectionOneImage.title} />
+          <LocationsList className="list-unstyled">
+            {sectionOneList.map(place => <LocationListItem key={place}>{place}</LocationListItem>)}
+          </LocationsList>
+        </ImageContainer>
+      </section>
+      <section>
+        <h2>{sectionTwoTitle}</h2>
+        <TeamMemberList className="d-flex flex-wrap">
+          {teamMembers.map(teamMember => (
+            <TeamMember
+              key={teamMember.name}
+              imageUrl={teamMember.image.url}
+              title={teamMember.name}
+              subtitle={teamMember.region}
+            />))
+          }
+        </TeamMemberList>
+      </section>
     </Container>
+    <Banner buttonLink="a" buttonText={bannerButtonText} headline={bannerTitle} />
   </Layout>
 )
 
-export default TeamDe
+TeamDe.propTypes = {
+  title: PropTypes.string.isRequired,
+  pageDescription: PropTypes.string.isRequired,
+  bannerButtonText: PropTypes.string.isRequired,
+  bannerTitle: PropTypes.string.isRequired,
+  sectionOneTitle: PropTypes.string.isRequired,
+  sectionOneText: PropTypes.string.isRequired,
+  sectionTwoTitle: PropTypes.string.isRequired,
+  sectionOneImage: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  sectionOneList: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  teamMembers: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired).isRequired,
+}
+
+TeamDe.getInitialProps = async function initialProps({ query }) {
+  return fetchTeamDePage(getLocaleFromQuery(query))
+}
+
+export default WithHeaderAndFooterData(TeamDe)
 
