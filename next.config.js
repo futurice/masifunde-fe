1e6a3badf7378ed20889b456186e97d9a5e8e644
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-11-21 17:37:17 +0100

    Generate routes from next-routes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const Routes = require('./routes')
+
+// It can't handle complex patterns like: /blog/:id/
+function createNextRoutesFromNextRoutes() {
+  return Routes.routes.reduce((map, { pattern, page }) => {
+    // eslint-disable-next-line no-param-reassign
+    map[pattern] = { page }
+    return map
+  }, {})
+}
+
 module.exports = {
   exportPathMap() {
-    return {
-      '/': { page: '/home' },
-      // '/en/waswirmachen': { page: '/waswirmachen', query: { locale: 'en' } },
-    }
+    return createNextRoutesFromNextRoutes()
   },
 }
