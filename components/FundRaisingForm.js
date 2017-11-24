271e5a5934eabc85a3a09d5f59d82af63cc1578b
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-11-24 10:07:03 +0100

    Add FundRaisingForm

---
+++ components/FundRaisingForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import ReactAsyncScript from 'react-async-script'
+import { stringify } from 'query-string'
+import PropTypes from 'prop-types'
+
+// It is necessary class component since it is required for ref in ReactAsyncScript
+// eslint-disable-next-line react/prefer-stateless-function
+class FundRaisingForm extends Component {
+  render() {
+    return (
+      <div>
+        <div id="fbIframeDiv" style={{ position: 'relative' }} />
+        <a target="_blank" rel="noopener noreferrer" href="http://www.fundraisingbox.com">
+          <img
+            border="0"
+            style={{ border: '0 !important' }}
+            src="https://secure.fundraisingbox.com/images/FundraisingBox-Logo-Widget.png"
+            alt="FundraisingBox Logo"
+          />
+        </a>
+      </div>
+    )
+  }
+}
+
+function ScriptParametersWrapper({ amount }) {
+  const URL = 'https://secure.fundraisingbox.com/app/paymentJS'
+  const parameters = {
+    hash: 'j3ip42zwp3mlewb9',
+    amount,
+  }
+
+  const Form = ReactAsyncScript(
+    FundRaisingForm,
+    `${URL}?${stringify(parameters)}`,
+    {
+      globalName: 'FundRaisingBox',
+      removeOnUnmount: true,
+    },
+  )
+
+  return <Form />
+}
+
+ScriptParametersWrapper.propTypes = {
+  amount: PropTypes.number,
+}
+
+ScriptParametersWrapper.defaultProps = {
+  amount: undefined,
+}
+
+export default ScriptParametersWrapper
