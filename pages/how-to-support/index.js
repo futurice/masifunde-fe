2a89883907d56166e6a4838294565f013191d3fc
Torkjel Skjetne <torkjel.skjetne@futurice.com>
2017-11-27 00:39:18 +0100

    Basic layout for How To Support page

--- pages/how-to-support/index.js
+++ pages/how-to-support/index.js
@@ -1,106 +1,157 @@
-/* eslint-disable react/prop-types */
 import React from 'react'
-import { Button, Container, Row, Col } from 'reactstrap'
+import { Container } from 'reactstrap'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
 import { fetchHowToSupportPage } from '../../api/howToSupport'
-import Layout from '../../components/Layout'
+import { getLocaleFromQuery } from '../../utils/locale'
+import LayoutWrapper from '../../components/LayoutWrapper'
+import Head from '../../components/Head'
+import Hero from '../../components/Hero'
+import Markdown from '../../components/Markdown'
 
-const buttonStyle = {
-  color: 'white',
-  backgroundColor: 'black',
-  border: 'none',
-  cursor: 'pointer',
-}
+const Paragraph = styled(Markdown)`
+  text-align: center;
+`
 
-const ovalStyle = {
-  width: '100',
-  height: '100',
-}
+const H2 = styled.h2`
+  text-align: left;
+`
 
-const rowStyle = {
-  marginTop: '10',
-  marginBottom: '100',
-}
-
-const topRowStyle = {
-  marginBottom: '100',
-  backgroundColor: 'Gainsboro',
-}
-
-const colOvalStyle = {
-  textAlign: 'right',
-}
-
-const titleStyle = {
-  marginTop: '50',
-  marginBottom: '50',
-}
-
-const Howtohelp = props => (
-  <Layout>
-    <Container>
-      <Row style={topRowStyle}>
-        {/* Section intro part */}
-        <Col xs="1" sm="1" md="1" lg="1" xl="1" />
-        <Col xs="6" sm="6" md="6" lg="6" xl="6" style={titleStyle}>
-          <h2>{props.title}</h2>
-        </Col>
-        <Col xs="5" sm="5" md="5" lg="5" xl="5" />
-      </Row>
-      <Row style={rowStyle}>
-        {/* Financial support part */}
-        <Col xs="3" sm="3" md="3" lg="3" xl="3" style={colOvalStyle}>
-          <img src="../static/oval4.svg" style={ovalStyle} alt="" />
-        </Col>
-        <Col xs="7" sm="7" md="7" lg="7" xl="7">
-          <h1>{props.paragraphOneTitle}</h1>
-          <p>{props.paragraphOneText}</p>
-          <Button color="primary" style={buttonStyle}>
-            Spenden
-          </Button>
-        </Col>
-        <Col xs="2" sm="2" md="2" lg="2" xl="2" />
-      </Row>
-      <Row style={rowStyle}>
-        {/* Volunteer support part */}
-        <Col xs="3" sm="3" md="3" lg="3" xl="3" style={colOvalStyle}>
-          <img src="../static/oval4.svg" style={ovalStyle} alt="" />
-        </Col>
-        <Col xs="7" sm="7" md="7" lg="7" xl="7">
-          <h1>{props.paragraphTwoTitle}</h1>
-          <p>{props.paragraphTwoText}</p>
-          <Button color="primary" style={buttonStyle}>
-            Learn about becoming a volunteer
-          </Button>
-        </Col>
-        <Col xs="2" sm="2" md="2" lg="2" xl="2" />
-      </Row>
-      <Row style={rowStyle}>
-        {/* Institutional support part */}
-        <Col xs="3" sm="3" md="3" lg="3" xl="3" style={colOvalStyle}>
-          <img src="../static/oval4.svg" style={ovalStyle} alt="" />
-        </Col>
-        <Col xs="7" sm="7" md="7" lg="7" xl="7">
-          <h1>{props.paragraphThreeTitle}</h1>
-          <p>{props.paragraphThreeText}</p>
-          <Button color="primary" style={buttonStyle}>
-            Unternehmenspartner werden
-          </Button>
-        </Col>
-        <Col xs="2" sm="2" md="2" lg="2" xl="2" />
-      </Row>
-      <Row>
-        <Col>
-          <img src={props.partnersImageUrl} alt="" />
-        </Col>
-      </Row>
-    </Container>
-  </Layout>
+const Section = ({
+  image, title, markdown, buttonText,
+}) => (
+  <SectionContainer>
+    <img src={image.url} alt={image.tile} />
+    <SectionContentContainer>
+      <H2>{title}</H2>
+      <Markdown source={markdown} />
+      <button>{buttonText}</button>
+    </SectionContentContainer>
+  </SectionContainer>
 )
 
-Howtohelp.getInitialProps = async function getInitialProps() {
-  const resultJSON = await fetchHowToSupportPage()
-  console.log('Result:', resultJSON)
-  return resultJSON
+const SectionContainer = styled.div`
+  display: flex;
+  padding: 3rem 0;
+`
+
+const SectionContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1.4rem;
+`
+
+Section.propTypes = {
+  image: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  title: PropTypes.string.isRequired,
+  markdown: PropTypes.string.isRequired,
+  buttonText: PropTypes.string.isRequired,
 }
 
-export default Howtohelp
+const HowToSupport = ({
+  metaTitle,
+  metaDescription,
+  heroTitle,
+  introHeading,
+  introMarkdown,
+  section1Image,
+  section1Title,
+  section1Markdown,
+  section1ButtonText,
+  section2Image,
+  section2Title,
+  section2Markdown,
+  section2ButtonText,
+  section3Image,
+  section3Title,
+  section3Markdown,
+  section3ButtonText,
+  section4Image,
+  section4Title,
+  section4Markdown,
+  section4ButtonText,
+}) => (
+  <div>
+    <Head title={metaTitle} description={metaDescription} />
+    <Hero headline={heroTitle} imageUrl="//via.placeholder.com/350x150/555" />
+    <Container>
+      <h1>{introHeading}</h1>
+      <Paragraph source={introMarkdown} />
+      <Section
+        buttonText={section1ButtonText}
+        title={section1Title}
+        markdown={section1Markdown}
+        image={section1Image}
+      />
+      <Section
+        buttonText={section2ButtonText}
+        title={section2Title}
+        markdown={section2Markdown}
+        image={section2Image}
+      />
+      <hr />
+      <Section
+        buttonText={section3ButtonText}
+        title={section3Title}
+        markdown={section3Markdown}
+        image={section3Image}
+      />
+      <Section
+        buttonText={section4ButtonText}
+        title={section4Title}
+        markdown={section4Markdown}
+        image={section4Image}
+      />
+    </Container>
+  </div>
+)
+
+HowToSupport.propTypes = {
+  metaTitle: PropTypes.string.isRequired,
+  metaDescription: PropTypes.string,
+  heroTitle: PropTypes.string.isRequired,
+  introHeading: PropTypes.string.isRequired,
+  introMarkdown: PropTypes.string.isRequired,
+  section1Image: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  section1Title: PropTypes.string.isRequired,
+  section1Markdown: PropTypes.string.isRequired,
+  section1ButtonText: PropTypes.string.isRequired,
+  section2Image: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  section2Title: PropTypes.string.isRequired,
+  section2Markdown: PropTypes.string.isRequired,
+  section2ButtonText: PropTypes.string.isRequired,
+  section3Image: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  section3Title: PropTypes.string.isRequired,
+  section3Markdown: PropTypes.string.isRequired,
+  section3ButtonText: PropTypes.string.isRequired,
+  section4Image: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  section4Title: PropTypes.string.isRequired,
+  section4Markdown: PropTypes.string.isRequired,
+  section4ButtonText: PropTypes.string.isRequired,
+}
+
+HowToSupport.defaultProps = {
+  metaDescription: undefined,
+}
+
+HowToSupport.getInitialProps = async function getInitialProps({ query }) {
+  return fetchHowToSupportPage(getLocaleFromQuery(query))
+}
+
+export default LayoutWrapper(HowToSupport)
