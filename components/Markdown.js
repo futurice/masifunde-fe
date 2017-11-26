9cb0ad32cc77ef252092c75e3c5c847e74254aff
Torkjel Skjetne <torkjel.skjetne@futurice.com>
2017-11-27 00:38:06 +0100

    PropType required for Markdown component

--- components/Markdown.js
+++ components/Markdown.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import PropTypes from 'prop-types'
 
 const Markdown = props => <ReactMarkdown {...props} softBreak="br" />
+
+Markdown.propTypes = {
+  source: PropTypes.string.isRequired,
+}
 
 export default Markdown
