9ff719a1e01e94af995e75cb601f1a702a73f824
Torkjel Skjetne <torkjel.skjetne@futurice.com>
2017-11-27 17:41:16 +0100

    Connect Contact page to new content Team Member

--- api/contact.js
+++ api/contact.js
@@ -1,18 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import { fetchSingleEntry } from './contentfulService'
+import { unwrapImage, unwrapRegion } from './common'
 
 export async function fetchContactPage(locale) {
   const content = await fetchSingleEntry('pageKontakt', locale)
-
+  const mapContact = contact => ({
+    ...contact.fields,
+    region: unwrapRegion(contact.fields.region),
+    image: unwrapImage(contact.fields.profileImage),
+  })
   return {
     ...content,
-    contacts: content.contacts.map(contact => ({
-      ...contact.fields,
-      imageUrl: contact.fields.image.fields.file.url,
-    })),
-    regionalContacts: content.regionalContacts.map(contact => ({
-      ...contact.fields,
-      imageUrl: contact.fields.image.fields.file.url,
-    })),
+    contacts: content.contacts.map(mapContact),
+    regionalContacts: content.regionalContacts.map(mapContact),
   }
 }
