98e3701b4ebad38b09adb2b4e4474a350f1ea5a3
Torkjel Skjetne <torkjel.skjetne@futurice.com>
2017-11-27 12:01:48 +0100

    Give Header a different background color than the rest of the page. Align nav items horizontally

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, Container } from 'reactstrap'
+import styled from 'styled-components'
 
 import { RouteNames } from '../../routes'
 import NavigationLink from './NavigationLink'
 import ButtonLink from './ButtonLink'
 
-const containerStyle = {
-  marginTop: 20,
-  marginBottom: 20,
-}
+const HeaderWrapper = styled.div`
+  background-color: #f4e3d6;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`
 
 class Header extends Component {
   state = {
@@ -28,24 +30,26 @@ class Header extends Component {
     } = this.props
 
     return (
-      <Container style={containerStyle}>
-        <Navbar color="faded" light expand="md">
-          <NavbarBrand href="/">
-            <Link href="/">
-              <img src="../static/Masifunde-Logo.png" alt="Masifunde Logo" />
-            </Link>
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavigationLink href={RouteNames.WhatWeDo}>{whatWeDoText}</NavigationLink>
-              <NavigationLink href={RouteNames.WhoWeAre}>{whoWeAreText}</NavigationLink>
-              <NavigationLink href={RouteNames.HowToSupport}>{howToSupportText}</NavigationLink>
-              <ButtonLink href={RouteNames.Donate}>{donateText}</ButtonLink>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </Container>
+      <HeaderWrapper>
+        <Container>
+          <Navbar color="faded" light expand="md">
+            <NavbarBrand href="/">
+              <Link href="/">
+                <img src="../static/Masifunde-Logo.png" alt="Masifunde Logo" />
+              </Link>
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto d-flex align-items-center" navbar>
+                <NavigationLink href={RouteNames.WhatWeDo}>{whatWeDoText}</NavigationLink>
+                <NavigationLink href={RouteNames.WhoWeAre}>{whoWeAreText}</NavigationLink>
+                <NavigationLink href={RouteNames.HowToSupport}>{howToSupportText}</NavigationLink>
+                <ButtonLink href={RouteNames.Donate}>{donateText}</ButtonLink>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </Container>
+      </HeaderWrapper>
     )
   }
 }
