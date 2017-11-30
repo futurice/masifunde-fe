6797a7f53a30afc7f33b0a9395c48c1fb7f66b4f
Torkjel Skjetne <torkjel.skjetne@futurice.com>
2017-11-30 18:14:54 +0100

    Connect Become A Volunteer page to Contentful

--- api/howToSupport.js
+++ api/howToSupport.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { fetchMemoizedSingleEntry } from './contentfulService'
-import { unwrapImage } from './common'
+import { unwrapImage, unwrapRegionalGroups, unwrapTeamMember } from './common'
 
 export async function fetchHowToSupportPage(locale) {
   const content = await fetchMemoizedSingleEntry('pageWieSieHelfen', locale)
@@ -10,5 +10,19 @@ export async function fetchHowToSupportPage(locale) {
     section2Image: unwrapImage(content.section2Image),
     section3Image: unwrapImage(content.section3Image),
     section4Image: unwrapImage(content.section4Image),
+  }
+}
+
+const unwrapVolunteerOpening = volunteerOpening => ({
+  ...volunteerOpening.fields,
+})
+
+export async function fetchBecomeAVolunteerPage(locale) {
+  const content = await fetchMemoizedSingleEntry('pageAktivWerden', locale)
+  return {
+    ...content,
+    regionalGroups: unwrapRegionalGroups(content.regionalGroups),
+    section1TeamMember: unwrapTeamMember(content.section1TeamMember),
+    volunteerOpenings: content.volunteerOpenings.map(unwrapVolunteerOpening),
   }
 }
