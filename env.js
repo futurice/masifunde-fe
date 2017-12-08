82ec3b901ce81e1d1d4e176d90f777598c406ebb
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-12-08 14:21:07 +0100

    Allow to pass contentful host

--- env.js
+++ env.js
@@ -2,3 +2,4 @@ export const CONTENTFUL_ACCESS_TOKEN =
   process.env.CONTENTFUL_ACCESS_TOKEN ||
   '5c8090d12bc2bf8dc695353cc398cd5e48eb56c214325884284bfdbfef4ba5ed'
 export const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID || '6jocdllnp50q'
+export const CONTENTFUL_HOST = process.env.CONTENTFUL_HOST || undefined
