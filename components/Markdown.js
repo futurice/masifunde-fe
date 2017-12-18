a14d26cfebab1dc899ec942ae77b0c865c9c1326
Torkjel Skjetne <tskjetne@gmail.com>
2017-12-18 13:01:42 +0100

    Open Markdown links in new tab

--- components/Markdown.js
+++ components/Markdown.js
@@ -1,8 +1,15 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import PropTypes from 'prop-types'
 
-const Markdown = props => <ReactMarkdown {...props} softBreak="br" />
+const Markdown = props => (
+  <ReactMarkdown
+    {...props}
+    softBreak="br"
+    renderers={{ Link: ({ href, children }) => <a href={href} target="_blank">{children}</a> }}
+  />
+)
 
 Markdown.propTypes = {
   source: PropTypes.string.isRequired,
