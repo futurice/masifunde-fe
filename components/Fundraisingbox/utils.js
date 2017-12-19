8aa5d334251477366fc2f2ca1043b118ec216a65
Torkjel Skjetne <tskjetne@gmail.com>
2017-12-19 16:31:47 +0100

    Util to validate positive integer

--- components/Fundraisingbox/utils.js
+++ components/Fundraisingbox/utils.js
@@ -1,2 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 export const isInvalid = meta => meta.error && meta.touched
+
+export const isPositiveInteger = (field) => {
+  const parsedNumber = Number(field)
+  const isInteger = Number.isInteger(parsedNumber)
+  return (isInteger && parsedNumber > 0)
+}
