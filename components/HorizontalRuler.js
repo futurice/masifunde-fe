3378bd08e584251384fb92883e337a57951127b7
Torkjel Skjetne <tskjetne@gmail.com>
2017-12-21 15:26:48 +0100

    Horizontal ruler on smaller screens

--- components/HorizontalRuler.js
+++ components/HorizontalRuler.js
@@ -2,7 +2,11 @@ import styled from 'styled-components'
 
 const HorizontalRuler = styled.hr`
   border-color: ${props => props.theme.orange};
-  margin: auto 22%;
+  margin: auto 12%;
+  
+  @media (min-width: 576px) {
+    margin: auto 22%;
+  }
 `
 
 export default HorizontalRuler
