9162bfce017e3685142c41b45c77d86e5e4af129
Torkjel Skjetne <tskjetne@gmail.com>
2017-12-21 15:09:09 +0100

    Shrank horizontal ruler

--- components/HorizontalRuler.js
+++ components/HorizontalRuler.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const HorizontalRuler = styled.hr`
   border-color: ${props => props.theme.orange};
-  margin: auto 13%;
+  margin: auto 22%;
 `
 
 export default HorizontalRuler
