304a66551bc268677283246f259d3610f2e9e057
Torkjel Skjetne <tskjetne@gmail.com>
2017-12-21 15:31:49 +0100

    Use a more proper word-breaking tactic for headers

--- styling/typography.js
+++ styling/typography.js
@@ -6,29 +6,42 @@ export const handwritten = css`
 `
 
 export const pageTitle = css`
-    word-break: break-all;
+    overflow-wrap: break-word;
     line-height: 1.2;
     margin: 4rem 0;
     color: #FF621D;
     letter-spacing: 0.5px;
+    font-size: 2.5rem;
+
+    @media (min-width: 768px) {
+      font-size: 3rem;
+    }
 `
 
 export const sectionTitle = css`
-    word-break: break-all;
+    overflow-wrap: break-word;
     line-height: 1.4;
     margin: 3rem 0;
     color: #FF621D;
-    font-size: 2.5rem;
+    font-size: 2rem;
     letter-spacing: -0.5px;
+
+    @media (min-width: 768px) {
+      font-size: 2.5rem;
+    }
 `
 
 export const subtitle = css`
-    word-break: break-all;
+    overflow-wrap: break-word;
     line-height: 1.4;
     color: #77695C;
     font-weight: normal;
-    font-size: 2rem;
+    font-size: 1.5rem;
     margin: 0 0 8px;
     text-align: left;
+
+    @media screen and (min-width: 768px) {
+      font-size: 2rem;
+    }
 `
 
