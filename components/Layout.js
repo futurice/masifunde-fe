35e4e8b8262ab8b0cd558240fa29ac9e79728f63
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-12-21 14:38:39 +0100

    Add favicons

--- components/Layout.js
+++ components/Layout.js
@@ -112,6 +112,8 @@ const Layout = ({ headerData, children, footerData }) => (
     <Fragment>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/static/favicon/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/static/favicon/favicon-16x16.png" sizes="16x16" />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0-beta.2/dist/css/bootstrap.min.css"
