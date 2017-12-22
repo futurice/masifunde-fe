f51fa7d96b527bb0498bb437926338bd0402b9fd
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-12-22 09:52:36 +0100

    Push theme files

---
+++ styling/theme.js
@@ -0,0 +1,14 @@
+export default {
+  orange: '#FE9933',
+  orangeRed: '#FF621D',
+  green: '#17DD73',
+  darkGreen: '#00C078',
+  blue: '#4176F9',
+  linkBlue: '#225DBC',
+  pineCone: '#77695C',
+  roundedImageBorderRadius: '6px',
+  error: '#DC3545',
+  headerHeight: '4.5rem',
+  pagePadding: '4rem',
+  pagePaddingMobile: '2.5rem',
+}
