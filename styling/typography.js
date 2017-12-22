40e1572a16110030f578571f8883d7f3f275f488
Torkjel Skjetne <tskjetne@gmail.com>
2017-12-22 10:59:24 +0100

    Fixed wordwrap IE11 bug and tweaked heading sizes

--- styling/typography.js
+++ styling/typography.js
@@ -6,7 +6,8 @@ export const handwritten = css`
 `
 
 export const pageTitle = css`
-    overflow-wrap: break-word;
+    word-wrap: break-word;
+    width: 100%;
     line-height: 1.2;
     color: #FF621D;
     letter-spacing: 0.5px;
@@ -14,12 +15,13 @@ export const pageTitle = css`
     margin: 0 0 4rem;
 
     @media (min-width: 768px) {
-      font-size: 3rem;
+      font-size: 2.7rem;
     }
 `
 
 export const sectionTitle = css`
-    overflow-wrap: break-word;
+    word-wrap: break-word;
+    width: 100%;
     line-height: 1.4;
     color: #FF621D;
     font-size: 2rem;
@@ -27,12 +29,13 @@ export const sectionTitle = css`
     margin: 0 0 3rem;
 
     @media (min-width: 768px) {
-      font-size: 2.5rem;
+      font-size: 2.4rem;
     }
 `
 
 export const subtitle = css`
-    overflow-wrap: break-word;
+    word-wrap: break-word;
+    width: 100%;
     line-height: 1.4;
     color: #77695C;
     font-weight: normal;
