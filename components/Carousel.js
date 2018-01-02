1a39374bae26ff06b22ec9b18a43705afe3c87a9
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-02 13:47:56 +0100

    Carousel image wider on medium screen

--- components/Carousel.js
+++ components/Carousel.js
@@ -158,9 +158,9 @@ class MasifundeCarousel extends Component {
         <StyledCarousel {...settings}>
           {items.map(item => (
             <SlideRow key={`${item.heading} ${item.image.url}`} className="row">
-              <Image className="d-none d-md-block col-md-3" src={`${item.image.url}?q=${jpgCompression}`} alt={item.image.title} />
-              <MobileImage className="d-md-none p-0 col-md-3 w-100 h-100" src={`${item.image.url}?q=${jpgCompression}`} alt={item.image.title} />
-              <CarouselTextContainer className="col-md-9">
+              <Image className="d-none d-md-block col-md-4 col-lg-3" src={`${item.image.url}?q=${jpgCompression}`} alt={item.image.title} />
+              <MobileImage className="d-md-none p-0 col-md-4 col-lg-3 w-100 h-100" src={`${item.image.url}?q=${jpgCompression}`} alt={item.image.title} />
+              <CarouselTextContainer className="col-md-8 col-lg-9">
                 <H3 className="row">{item.heading}</H3>
                 <Markdown className="row" source={item.text} />
               </CarouselTextContainer>
