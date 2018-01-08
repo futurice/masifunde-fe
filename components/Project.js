cf375d4c962eb4eac0699c953f175fe153a964a1
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2018-01-08 16:23:15 +0100

    Add jpg compression to Projects

--- components/Project.js
+++ components/Project.js
@@ -2,8 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Markdown from 'react-markdown'
 import styled from 'styled-components'
+
 import imageShape from '../propTypes/image'
 import { smallFontSize } from '../styling/typography'
+import { jpgCompression } from '../utils/constants'
 
 const ProjectImage = styled.img`
   max-width: 100%;
@@ -27,7 +29,7 @@ const Project = ({
   className, image, name, description,
 }) => (
   <ProjectContainer className={className}>
-    <ProjectImage className="mx-auto d-block" src={image.url} alt={image.tile} />
+    <ProjectImage className="mx-auto d-block" src={`${image.url}?q=${jpgCompression}`} alt={image.title} />
     <H3>{name}</H3>
     <Markdown source={description} />
   </ProjectContainer>)
