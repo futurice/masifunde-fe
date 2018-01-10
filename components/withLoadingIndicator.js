dea41b7596fa3d0fcc72df5588f763f3dcbc8559
Denis Washington <denis.washington@futurice.com>
2018-01-10 15:11:49 +0100

    Reload page on version update
    This is normally done by Next.js automatically, but is aparently
    not working for statically exported pages, causing the site to
    get stuck when trying to follow a link after the website has been
    updated in the background.
    
    https://github.com/zeit/next.js/issues/3558
    
    As a workaround, we speculatively do a full reload five seconds after
    a route change has started but not completed. To the user, this will
    seem like just a particularly long page load.

--- components/withLoadingIndicator.js
+++ components/withLoadingIndicator.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import NProgress from 'nprogress'
-import Router from 'next/router'
 import { injectGlobal } from 'styled-components'
 
 import theme from '../styling/theme'
+import {
+  onRouteChangeStart,
+  onRouteChangeComplete,
+  onRouteChangeError,
+} from '../utils/routerEvents'
 
 const PROGRESS_COLOR = theme.orangeRed
 
@@ -15,16 +19,16 @@ injectGlobal`
 
   #nprogress .bar {
     background: ${PROGRESS_COLOR};
-  
+
     position: fixed;
     z-index: 1031;
     top: 0;
     left: 0;
-  
+
     width: 100%;
     height: 2px;
   }
-  
+
   /* Fancy blur effect */
   #nprogress .peg {
     display: block;
@@ -34,12 +38,12 @@ injectGlobal`
     height: 100%;
     box-shadow: 0 0 10px ${PROGRESS_COLOR}, 0 0 5px ${PROGRESS_COLOR};
     opacity: 1.0;
-  
+
     -webkit-transform: rotate(3deg) translate(0px, -4px);
         -ms-transform: rotate(3deg) translate(0px, -4px);
             transform: rotate(3deg) translate(0px, -4px);
   }
-  
+
   /* Remove these to get rid of the spinner */
   #nprogress .spinner {
     display: block;
@@ -48,31 +52,31 @@ injectGlobal`
     top: 15px;
     right: 15px;
   }
-  
+
   #nprogress .spinner-icon {
     width: 18px;
     height: 18px;
     box-sizing: border-box;
-  
+
     border: solid 2px transparent;
     border-top-color: ${PROGRESS_COLOR};
     border-left-color: ${PROGRESS_COLOR};
     border-radius: 50%;
-  
+
     -webkit-animation: nprogress-spinner 400ms linear infinite;
             animation: nprogress-spinner 400ms linear infinite;
   }
-  
+
   .nprogress-custom-parent {
     overflow: hidden;
     position: relative;
   }
-  
+
   .nprogress-custom-parent #nprogress .spinner,
   .nprogress-custom-parent #nprogress .bar {
     position: absolute;
   }
-  
+
   @-webkit-keyframes nprogress-spinner {
     0%   { -webkit-transform: rotate(0deg); }
     100% { -webkit-transform: rotate(360deg); }
@@ -88,9 +92,9 @@ NProgress.configure({
   trickleSpeed: 100,
 })
 
-Router.onRouteChangeStart = () => NProgress.start()
-Router.onRouteChangeComplete = () => NProgress.done()
-Router.onRouteChangeError = () => NProgress.done()
+onRouteChangeStart(() => NProgress.start())
+onRouteChangeComplete(() => NProgress.done())
+onRouteChangeError(() => NProgress.done())
 
 const withLoaderIndicator = (WrappedComponent) => {
   const LoadingIndicatorWrapper = props => (
