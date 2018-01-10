a74b2c68cc02da166001de65d21982c212ff60f8
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-10 18:40:46 +0100

    Fix fundraisingform formlabel padding on larger screens

--- components/Fundraisingbox/FundRaisingForm.js
+++ components/Fundraisingbox/FundRaisingForm.js
@@ -19,11 +19,14 @@ const FundRaisingIfameContainer = styled.div`
 const FormLabel = styled.span`
   text-align: left;
   padding-right: 0;
-  padding-top: 0;
-  padding-bottom: 0;
 
   @media screen and (min-width: ${mdBreakpoint}){
     text-align: right;
+  }
+
+  @media (max-width: 767.99px){
+    padding-top: 0;
+    padding-bottom: 0;
   }
 `
 
