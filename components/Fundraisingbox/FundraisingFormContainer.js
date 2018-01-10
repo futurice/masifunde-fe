21cd9b354b76f70386036fa644b30ea69e3b5f2c
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-10 17:48:37 +0100

    Add FundraisingFormContainer to reduce width of form

---
+++ components/Fundraisingbox/FundraisingFormContainer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { lgBreakpoint } from '../../styling/breakpoints'
+
+const SubContainer = styled.div`
+  margin: 0 auto;
+
+  @media (min-width: ${lgBreakpoint}) {
+    width: 60%;
+  }
+  
+  ${({ pullLeft }) => pullLeft && 'margin-left: 0;'}
+`
+
+const FundraisingFormContainer = ({ children, pullLeft }) => (
+  <Container>
+    <SubContainer pullLeft={pullLeft}>
+      {children}
+    </SubContainer>
+  </Container>
+)
+
+FundraisingFormContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+  pullLeft: PropTypes.bool,
+}
+
+FundraisingFormContainer.defaultProps = {
+  pullLeft: false,
+}
+
+export default FundraisingFormContainer
