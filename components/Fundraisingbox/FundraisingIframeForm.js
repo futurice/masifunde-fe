5df1ac88422a6d14975846ba026f12672714311d
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-11 14:16:39 +0100

    Pull FunraidingIframeForm out to its own component

---
+++ components/Fundraisingbox/FundraisingIframeForm.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import FundraisingboxLink from './FundraisingboxLink'
+import PageSection from '../PageSection'
+import SubHeader from './SubHeader'
+import FundraisingIframe from './FundRaisingIframe'
+import FundraisingFormContainer from './FundraisingFormContainer'
+
+const FundraisingIframeForm = ({
+  fields,
+  formTitle,
+  hash,
+  hiddenFields,
+  onMouseHover,
+  pullLeft,
+}) => (
+  <PageSection contained={false}>
+    <FundraisingFormContainer pullLeft={pullLeft}>
+      <SubHeader>{formTitle}</SubHeader>
+    </FundraisingFormContainer>
+    <FundraisingIframe
+      onMouseHover={onMouseHover}
+      hash={hash}
+      {...fields}
+      {...hiddenFields}
+    />
+    <FundraisingFormContainer pullLeft={pullLeft}>
+      <FundraisingboxLink />
+    </FundraisingFormContainer>
+  </PageSection>
+)
+
+FundraisingIframeForm.propTypes = {
+  fields: PropTypes.shape(),
+  formTitle: PropTypes.string.isRequired,
+  hash: PropTypes.string.isRequired,
+  hiddenFields: PropTypes.shape().isRequired,
+  onMouseHover: PropTypes.func.isRequired,
+  pullLeft: PropTypes.bool,
+}
+
+FundraisingIframeForm.defaultProps = {
+  fields: undefined,
+  pullLeft: false,
+}
+
+export default FundraisingIframeForm
