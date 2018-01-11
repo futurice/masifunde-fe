62f6f85df764271322864bbb787ca37116f852fb
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-11 13:59:20 +0100

    Created component for Fundraisingbox link

---
+++ components/Fundraisingbox/FundraisingboxLink.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FundraisingboxLink = () => (
+  <a target="_blank" rel="noopener noreferrer" href="http://www.fundraisingbox.com">
+    <img
+      border="0"
+      style={{ border: '0 !important' }}
+      src="https://secure.fundraisingbox.com/images/FundraisingBox-Logo-Widget.png"
+      alt="FundraisingBox Logo"
+    />
+  </a>
+)
+
+export default FundraisingboxLink
