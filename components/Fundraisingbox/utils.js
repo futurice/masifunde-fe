9f62721442aaaf3eeb6edfd506dee33772172151
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-15 13:24:26 +0100

    Create components for donation input fields to combat code duplication.

--- components/Fundraisingbox/utils.js
+++ components/Fundraisingbox/utils.js
@@ -1,7 +1,11 @@
-/* eslint-disable import/prefer-default-export */
-
 export const isPositiveInteger = (field) => {
   const parsedNumber = Number(field)
   const isInteger = Number.isInteger(parsedNumber)
   return (isInteger && parsedNumber > 0)
 }
+
+export const formInputClassFactory = meta =>
+  `form-control ${meta.error && meta.touched ? 'is-invalid' : ''}`
+
+export const formLabelBootstrapClasses = 'col-md-3'
+export const formInputBootstrapClasses = 'col-md-8'
