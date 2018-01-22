38aca5c187f26723e366bfa34d3f90724cfeb42d
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-22 16:18:27 +0100

    Remove donation page Page Section right padding of not contained

--- components/DonationForm/PageSection.js
+++ components/DonationForm/PageSection.js
@@ -3,6 +3,9 @@ import PageSection from '../PageSection'
 
 const DonationFormPageSection = styled(PageSection)`
   margin-top: 3rem;
+  
+  // Excplicitly checking false instead of falsy to not add the rule if 'contained' is undefined
+  ${({ contained }) => contained !== false && 'padding-right: 0;'}
 `
 
 export default DonationFormPageSection
