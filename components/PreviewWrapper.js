9d5d0064ed251276c06d839a2579f9099c58f1ee
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-25 14:49:00 +0100

    Document the purpose and behavior of the PreviewWrapper

--- components/PreviewWrapper.js
+++ components/PreviewWrapper.js
@@ -3,6 +3,17 @@ import React from 'react'
 
 import { IS_PREVIEW } from '../env'
 
+// Checks if we're currently running on the preview environment.
+// If we are, fetch all the data directly from Contentful.
+//
+// This is to make sure the page requested from the server is displaying the most up to date
+// Contentful data - not the data that was in Contentful when the static page was generated.
+// The generation and deploy of the site can take several minutes when doing many updates in
+// Contentful, which forces content editors to wait in order to preview their changes.
+// This wrapper makes the changes instantly appear on the page in the preview environment.
+//
+// Because the page is first shown with the content from the server rendered page, the old content
+// will be displayed briefly before being overwritten by the newer content fetched from Contentful.
 export default function PreviewWrapper(Page) {
   class GetInitialPropsWrapper extends React.Component {
     state = {}
