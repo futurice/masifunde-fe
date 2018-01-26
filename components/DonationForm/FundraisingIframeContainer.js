a72c53b2293b7e7cec6e7ac2cb9f42be7f7747d1
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2018-01-26 11:36:39 +0100

    Fixed Iframe scroll by adding hidden div The header height had to be addressed for anchor tag. Otherwise part of the iframe would be hidden behind header after scroll.

--- components/DonationForm/FundraisingIframeContainer.js
+++ components/DonationForm/FundraisingIframeContainer.js
@@ -1,33 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
 import FundraisingboxLink from './FundraisingboxLink'
 import PageSection from '../PageSection'
 import SubHeader from './SubHeader'
 import FundraisingIframe from './FundraisingIframe'
 import FormContainer from './FormContainer'
 
-const FundraisingIframeContainer = ({
-  fields,
-  formTitle,
-  ...rest
-}) => (
-  <PageSection contained={false}>
-    <FormContainer>
-      <SubHeader>{formTitle}</SubHeader>
-    </FormContainer>
-    <FundraisingIframe
-      {...fields}
-      {...rest}
-    />
-    <FormContainer>
-      <FundraisingboxLink />
-    </FormContainer>
-  </PageSection>
-)
+const ScrollDiv = styled.div`
+  position: relative;
+  top: -${({ theme }) => theme.headerHeight};
+`
+
+const IFRAME_SCROLL_ID = 'iframe_scroll_anchor'
+
+class FundraisingIframeContainer extends Component {
+  componentWillReceiveProps = (newProps) => {
+    if (newProps.scrollToIframe && !this.props.scrollToIframe) {
+      window.location.hash = IFRAME_SCROLL_ID
+    }
+  }
+
+  render() {
+    const {
+      fields,
+      formTitle,
+      scrollToIframe,
+      ...rest
+    } = this.props
+
+    return (
+      <PageSection contained={false}>
+        {!scrollToIframe && (
+          <FormContainer>
+            <SubHeader>{formTitle}</SubHeader>
+          </FormContainer>
+        )}
+        <ScrollDiv id={IFRAME_SCROLL_ID} />
+        <FundraisingIframe
+          {...fields}
+          {...rest}
+        />
+        <FormContainer>
+          <FundraisingboxLink />
+        </FormContainer>
+      </PageSection>
+    )
+  }
+}
 
 FundraisingIframeContainer.propTypes = {
   fields: PropTypes.shape(),
   formTitle: PropTypes.string.isRequired,
+  scrollToIframe: PropTypes.bool.isRequired,
 }
 
 FundraisingIframeContainer.defaultProps = {
