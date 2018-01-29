cac9e4fa0f85f0a1da25675de4f382808435be18
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-29 12:25:28 +0100

    Remove memoization from header and footer content fetching

--- api/common.js
+++ api/common.js
@@ -1,15 +1,15 @@
 import qs from 'qs'
 
-import { fetchMemoizedSingleEntry } from './contentfulService'
+import { fetchSingleEntry } from './contentfulService'
 import { jpegQuality } from '../utils/constants'
 import { RouteNames } from '../routes'
 
 export async function fetchHeaderData(locale) {
-  return fetchMemoizedSingleEntry('header', locale)
+  return fetchSingleEntry('header', locale)
 }
 
 export async function fetchFooterData(locale) {
-  return fetchMemoizedSingleEntry('footer', locale)
+  return fetchSingleEntry('footer', locale)
 }
 
 // For a list of valid image URL parameters, see the Contentful API docs:
