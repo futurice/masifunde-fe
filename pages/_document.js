3a6ff68311fc39e5c4d0a93d1fddc97c01e1b9d2
Torkjel Skjetne <tskjetne@gmail.com>
2018-01-29 17:45:08 +0100

    _document reuse getLocale functionality

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { getLocaleFromQuery } from '../utils/locale'
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage, query }) {
-    const lang = query && query.locale === 'en' ? 'en' : 'de'
+    const lang = getLocaleFromQuery(query)
 
     const sheet = new ServerStyleSheet()
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
