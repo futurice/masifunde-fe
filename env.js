1b7bc08b6bebbe1cff325ab9f6d33bcc0a6e5594
Denis Washington <denis.washington@futurice.com>
2018-02-08 14:58:11 +0100

    Add static routes for blog posts

--- env.js
+++ env.js
@@ -1,7 +1,13 @@
-export const CONTENTFUL_ACCESS_TOKEN =
-  process.env.CONTENTFUL_ACCESS_TOKEN ||
-  '5c8090d12bc2bf8dc695353cc398cd5e48eb56c214325884284bfdbfef4ba5ed'
-export const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID || '6jocdllnp50q'
-export const CONTENTFUL_HOST = process.env.CONTENTFUL_HOST || undefined
-export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || undefined
-export const IS_PREVIEW = process.env.IS_PREVIEW || undefined
+module.exports = {
+  // Contentful
+  CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID || '6jocdllnp50q',
+  CONTENTFUL_HOST: process.env.CONTENTFUL_HOST || undefined,
+  CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN ||
+    '5c8090d12bc2bf8dc695353cc398cd5e48eb56c214325884284bfdbfef4ba5ed',
+
+  // Google Analytics
+  GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID || undefined,
+
+  // Preview mode
+  IS_PREVIEW: process.env.IS_PREVIEW || undefined,
+}
