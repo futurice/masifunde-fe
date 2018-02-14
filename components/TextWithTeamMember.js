812be18fbdc5932377796e091a5cb6fb7589c891
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2018-02-14 17:29:10 +0100

    Change TextWithTeamMember component to new design

--- components/TextWithTeamMember.js
+++ components/TextWithTeamMember.js
@@ -5,32 +5,42 @@ import Markdown from './Markdown'
 import TeamMember from './TeamMember'
 import teamMemberShape from '../propTypes/teamMember'
 
-
 const TextWithTeamMember = ({
+  header,
   text,
   teamMember,
-  title,
-  subtitle,
+  teamMemberTitle,
+  teamMemberSubtitle,
 }) => (
   <div className="row">
-    <div className="col-md-9">
-      <Markdown source={text} />
+    <div className="col-lg-8 offset-lg-2">
+      <div className="row">
+        <div className="col-md-9">
+          { header && <h3>{header}</h3> }
+          <Markdown source={text} />
+        </div>
+        <TeamMember
+          className="col-md-3"
+          imageUrl={teamMember.image.url}
+          title={teamMemberTitle}
+          subtitle={teamMemberSubtitle}
+          email={teamMember.email}
+        />
+      </div>
     </div>
-    <TeamMember
-      className="col-md-3"
-      imageUrl={teamMember.image.url}
-      title={title}
-      subtitle={subtitle}
-      email={teamMember.email}
-    />
   </div>
 )
 
 TextWithTeamMember.propTypes = {
+  header: PropTypes.string,
   text: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
+  teamMemberTitle: PropTypes.string.isRequired,
+  teamMemberSubtitle: PropTypes.string.isRequired,
   teamMember: PropTypes.shape(teamMemberShape).isRequired,
+}
+
+TextWithTeamMember.defaultProps = {
+  header: undefined,
 }
 
 export default TextWithTeamMember
