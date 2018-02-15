ffe719efdd3f15471eac3ffd96037de1365bdaae
Torkjel Skjetne <tskjetne@gmail.com>
2018-02-15 17:09:05 +0100

    Fixed TeamMember size bug. Added word-break rules to wordBreak mixin

--- styling/utils.js
+++ styling/utils.js
@@ -8,6 +8,9 @@ export const wordBreak = css`
   /* These are technically the same, but use both for broader browser support */
   overflow-wrap: break-word;
   word-wrap: break-word;
+  
+  word-break: break-all;
+  word-break: break-word;
 
   /* Adds a hyphen where the word breaks, if supported (No Blink) */
   hyphens: auto;
