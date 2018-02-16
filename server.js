15ed6fda3c98b74e278ca3b690ca27e9ab2db590
Denis Washington <denis.washington@futurice.com>
2018-02-16 10:52:21 +0100

    Autodelete cache if dev server is run with ENABLED_FEATURES
    This ensures that changes to ENABLED_FEATURES are actually
    taken into account.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const { createServer } = require('http')
 const next = require('next')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const rimraf = require('rimraf')
 const routes = require('./routes')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handler = routes.getRequestHandler(app)
+
+// If the ENABLED_FEATURES environment variable is set, clear the babel-loader
+// cache to ensure that the feature flag configuration ends up in the compiled
+// bundle.
+if (typeof process.env.ENABLED_FEATURES === 'string') {
+  rimraf.sync(`${__dirname}/node_modules/.cache/babel-loader`)
+}
 
 app.prepare()
   .then(() => {
