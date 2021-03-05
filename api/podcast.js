9e1ba12603e83b409ce1660154515e4623644979
pbur <patrick.burden@futurice.com>
2021-03-05 17:06:26 +0100

    order of podcasts controlled by date2 field

--- api/podcast.js
+++ api/podcast.js
@@ -14,7 +14,8 @@ export function fetchPodcastList(locale, page) {
     locale,
     skip,
     limit: podcastPerPage,
-    order: '-fields.date',
+    //using date2 until this can be cleaned up in contenful as well
+    order: '-fields.date2',
   }
   return fetchEntriesForContentType('podcast', fetchParams)
   .then((response) => {
