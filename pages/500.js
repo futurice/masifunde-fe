d5f6a16a0ce712b68f287e22185e465836eb0f9a
Denis Washington <denis@denisw.de>
2022-12-03 19:11:51 +0100

    Upgrade to Next.js 10.2

---
+++ pages/500.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { getLayoutProps } from '../components/Layout'
+import Head from '../components/Head'
+import PageSection from '../components/PageSection'
+import Markdown from '../components/Markdown'
+import { fetchErrorPage500 } from '../api/500'
+
+const StyledMarkdown = styled(Markdown)`
+  text-align: center;
+`
+
+const ErrorPage500 = ({ metaTitle, title, descriptionMarkdown }) => (
+  <Fragment>
+    <Head title={metaTitle} />
+
+    <PageSection>
+      <h1>{title}</h1>
+      <StyledMarkdown source={descriptionMarkdown} />
+    </PageSection>
+  </Fragment>
+)
+
+ErrorPage500.propTypes = {
+  metaTitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  descriptionMarkdown: PropTypes.string.isRequired,
+}
+
+export async function getStaticProps() {
+  return {
+    // We cannot get the `locale` query parameter in `getStaticProps()`.
+    // Use only the German version for now.
+    props: {
+      ...(await getLayoutProps('de')),
+      ...(await fetchErrorPage500('de')),
+    },
+  }
+}
+
+export default ErrorPage500
